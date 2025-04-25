a4f281a8026570ffae60dd3231f445d74de4ebca
00016777 <wiut00016777@gmail.com>
2025-04-25 13:44:43 +0500

    feat: add solution to copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,15 @@
-const copy = async () => {
-    // Write your code here 
+import fs from 'node:fs';
+import fsPromises from 'node:fs/promises'
+
+const copy=async () => {
+    const filesCopyFolderPath = './src/fs/files_copy';
+    const filesFolderPath='./src/fs/files';
+    
+    if (fs.existsSync(filesCopyFolderPath)|| !fs.existsSync(filesFolderPath)) {
+        throw new Error('FS operation failed');
+    } else {
+        await fsPromises.cp(filesFolderPath, filesCopyFolderPath, {recursive: true});
+    }
 };
 
 await copy();
