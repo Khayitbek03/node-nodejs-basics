fca22417ef9e93f9c748cb4caff9dbfa197ce691
00016777 <wiut00016777@gmail.com>
2025-04-25 14:01:09 +0500

    feat: add solution to rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
-const rename = async () => {
-    // Write your code here 
+import fs from 'node:fs';
+import {rename as renameFile} from 'node:fs/promises';
+
+const rename=async () => {
+    const oldPath = './src/fs/files/wrongFilename.txt';
+    const newPath='./src/fs/files/properFileName.md';
+
+    if (fs.existsSync(newPath)||!fs.existsSync(oldPath)) {
+        throw new Error('FS operation failed');
+    } else {
+        await renameFile(oldPath, newPath);
+    }
 };
 
 await rename();
