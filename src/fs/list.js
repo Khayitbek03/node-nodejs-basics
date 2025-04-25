35976469e60199bf5f2d9cdf8dd4e4a81280f642
00016777 <wiut00016777@gmail.com>
2025-04-25 14:49:02 +0500

    feat: add solution to list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
-const list = async () => {
-    // Write your code here 
+import fs from 'node:fs';
+import fsPromises from 'node:fs/promises';
+
+const list=async () => {
+    const filesPath='./src/fs/files';
+    if (!fs.existsSync(filesPath)) {
+        throw Error('FS operation failed');
+    } else {
+        const files = await fsPromises.readdir(filesPath);
+        console.log(files);
+    }
 };
 
 await list();
