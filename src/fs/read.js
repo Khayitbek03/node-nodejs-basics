7875d1cef19bf08ac2e3d2104fe655ccf0bc018f
00016777 <wiut00016777@gmail.com>
2025-04-25 14:58:15 +0500

    feat: add solution to read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
-const read = async () => {
-    // Write your code here 
+import fs from 'node:fs'
+import fsPromises from 'node:fs/promises';
+
+const read=async () => {
+    const filePath='./src/fs/files/fileToRead.txt';
+    if (!fs.existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    } else {
+        const content=await fsPromises.readFile(filePath, 'utf-8');
+        console.log(content);
+    }
 };
 
 await read();
