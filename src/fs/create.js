c3e7c93812f0cd3b7556809c86e54427bdcf0613
00016777 <wiut00016777@gmail.com>
2025-04-25 13:16:04 +0500

    feat: add solution to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
-const create = async () => {
-    // Write your code here 
+import {writeFile} from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+
+const create=async () => {
+    const filePath='./src/fs/files/fresh.txt';
+    if (existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    } else {
+        const content='I am fresh and young';
+        await writeFile(filePath, content);
+    }
 };
 
 await create();
