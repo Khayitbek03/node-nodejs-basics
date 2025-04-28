15917aff5e3ccda92b3ec37c596ec39f7d28ef9b
Khayitbek Mirsoatov <xayitbekmirsoatov@gmail.com>
2025-04-28 14:22:54 +0500

    feat: add solution to hash folder

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
-const calculateHash = async () => {
-    // Write your code here 
+import {createHash} from 'crypto';
+import {createReadStream} from 'fs';
+
+const calculateHash=async () => {
+    const filePath='./src/hash/files/fileToCalculateHashFor.txt';
+    const hash=createHash('sha256');
+    const stream=createReadStream(filePath);
+    
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    stream.on('error', (err) => {
+        console.error('Error reading file:', err);
+    });
+    
+    stream.on('end', () => {
+        const res=hash.digest('hex');
+        console.log(res);
+    })
 };
 
 await calculateHash();
